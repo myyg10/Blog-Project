d67b3c4c586d295a4eae58c8f230ab0725ef2957
myyg10 <myyg10@naver.com>
2022-04-07 00:46:59 +0900

    로그인 구현 (시큐리티 X)

--- src/main/java/com/caholic/blog/controller/api/UserApiController.java
+++ src/main/java/com/caholic/blog/controller/api/UserApiController.java
@@ -1,21 +1,24 @@
 package com.caholic.blog.controller.api;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.caholic.blog.dto.ResponseDto;
 import com.caholic.blog.model.RoleType;
 import com.caholic.blog.model.User;
 import com.caholic.blog.service.UserService;
-
 @RestController
 public class UserApiController {
-
+	
 	@Autowired
 	private UserService userService;
+
+	@Autowired
+	private HttpSession session;
 
 	@PostMapping("/api/user")
 	public ResponseDto<Integer> save(@RequestBody User user) { // username, password, email
@@ -24,5 +27,16 @@ public class UserApiController {
 		user.setRole(RoleType.USER);
 		userService.회원가입(user);
 		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1); // 자바오브젝트를 JSON으로 변환해서 리턴 (Jackson)
+	}
+
+	@PostMapping("/api/user/login")
+	public ResponseDto<Integer> login(@RequestBody User user){
+		System.out.println("UserApiController : login호출됨");
+		User principal = userService.로그인(user); // principal (접근주체)
+
+		if(principal != null) {
+			session.setAttribute("principal", principal);
+		}
+		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
 	}
 }
