8c3a23e05db33121deb7bbfe127740bce3efda69
myyg10 <myyg10@naver.com>
2022-04-07 15:57:38 +0900

    회원 정보 수정 완료

--- src/main/java/com/caholic/blog/controller/UserController.java
+++ src/main/java/com/caholic/blog/controller/UserController.java
@@ -1,22 +1,33 @@
 package com.caholic.blog.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-//인증이 안된 사용자들이 출입할 수 있는 경로를 /auth/** 허용
-//그냥 주소가 / 이면 index.jsp 허용
-//static이하에 있는 /js/**, /css/**, /image/**
+import com.caholic.blog.config.auth.PrincipalDetail;
+
+// 인증이 안된 사용자들이 출입할 수 있는 경로를 /auth/** 허용
+// 그냥 주소가 / 이면 index.jsp 허용
+// static이하에 있는 /js/**, /css/**, /image/**
 
 @Controller
 public class UserController {
 
+
+
 	@GetMapping("/auth/joinForm")
-	public String joinForm() {	
+	public String joinForm() {
 		return "user/joinForm";
 	}
+	
 
 	@GetMapping("/auth/loginForm")
-	public String loginForm() {	
+	public String loginForm() {
 		return "user/loginForm";
+	}
+
+	@GetMapping("/user/updateForm")
+	public String updateForm() {	
+	  return "user/updateForm";
 	}
 }
